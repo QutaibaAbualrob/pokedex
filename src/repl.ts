6a14c9f90162852ae8d8dbbdd17905fe9d65245f
qutaiba_abualrob <qotaibawaleed@gmail.com>
2026-07-29 22:55:42 +0300

    add CLI command structure and implement help and exit commands
    create seperate files  command types, exit commands, help commands

--- src/repl.ts
+++ src/repl.ts
@@ -1,28 +1,43 @@
-import { createInterface } from 'node:readline';
+import { createInterface } from "node:readline";
+import { getCommands } from "./commands.js";
 
 export function startREPL() {
-    const rl = createInterface({
-        input: process.stdin,
-        output: process.stdout,
-        prompt: "Pokedex > ",
-    });
+  const commands = getCommands();
+  const rl = createInterface({
+    input: process.stdin,
+    output: process.stdout,
+    prompt: "Pokedex > ",
+  });
 
+  rl.prompt();
+
+  rl.on("line", (input: string) => {
+    const words = cleanInput(input);
+
+    if (words.length === 0) {
+      rl.prompt();
+      return;
+    }
+
+    const commandName = words[0];
+    const command = commands[commandName];
+
+    if (command) {
+      try {
+        command.callback(commands);
+      } catch (err) {
+        console.error(err);
+      }
+    } else {
+      console.log("Unknown command");
+    }
     rl.prompt();
-
-    rl.on("line", (input: string) => {
-        const words = cleanInput(input);
-        if (words.length === 0) {
-            rl.prompt();
-            return;
-        }
-        console.log(`Your command was: ${words[0]}`);
-        rl.prompt();
-    });
+  });
 }
 
 export default function cleanInput(input: string): string[] {
-    return input
-        .split(" ")
-        .filter(word => word !== "")
-        .map(word => word.toLowerCase());
+  return input
+    .split(" ")
+    .filter((word) => word !== "")
+    .map((word) => word.toLowerCase());
 }
