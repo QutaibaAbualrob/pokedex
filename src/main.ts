6a6e8a5f588f550a02a8f20787c60b78cc7236cf
qutaiba_abualrob <qotaibawaleed@gmail.com>
2026-07-29 22:46:12 +0300

    create input/output interface
    log the the user prompts
    
    ignore repl logs

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,9 @@
+import { startREPL } from "./repl.js";
+
 export function main() {
-  console.log("Hello, world!");
+  startREPL();
 }
+
+main();
 
 
--- src/repl.ts
+++ src/repl.ts
@@ -1,7 +1,28 @@
+import { createInterface } from 'node:readline';
+
+export function startREPL() {
+    const rl = createInterface({
+        input: process.stdin,
+        output: process.stdout,
+        prompt: "Pokedex > ",
+    });
+
+    rl.prompt();
+
+    rl.on("line", (input: string) => {
+        const words = cleanInput(input);
+        if (words.length === 0) {
+            rl.prompt();
+            return;
+        }
+        console.log(`Your command was: ${words[0]}`);
+        rl.prompt();
+    });
+}
+
 export default function cleanInput(input: string): string[] {
     return input
         .split(" ")
         .filter(word => word !== "")
         .map(word => word.toLowerCase());
 }
-
